b1b0ec2a865a6cdd369d51dbe3ce323e51c8eb0e
Davidov17 <baurjandavidov1@gmail.com>
2026-01-07 01:36:38 +0500

    Hello

---
+++ Lesson 2/Lesson-2video.py
@@ -0,0 +1,16 @@
+# # age = input("Your age is : ")
+# # print(f"your age is {age}")
+# # my_str = "apple"
+# # my_str = "A" + my_str[1:]
+# # print(my_str)
+#
+# int Sum = 123523
+# int Sum[1] = 4
+# print(Sum)
+
+fruit = "apple"
+print(fruit.lower())
+print(fruit.upper()[2])
+print(fruit.title())
+print(fruit.capitalize())
+print(fruit.endswith("e"))
